e144c0d0aa7f7d385ba6ed98e2203707c7b71178
Mohammad Zarif <m.zarif1996@gmail.com>
2022-03-04 23:55:26 +0330

    add UserManagerRespone to Shared project

---
+++ Asp.Net.Identity.shared/UserManagerResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Asp.Net.Identity.shared;
+
+public class UserManagerResponse
+{
+    public string Message { get; set; }
+    
+    public bool IsSuccess { get; set; }
+
+    public IEnumerable<string> Error { get; set; }
+}
