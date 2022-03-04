7cdef75ddea5496cbc7f906e6ba966a394443659
Mohammad Zarif <m.zarif1996@gmail.com>
2022-03-04 23:55:42 +0330

    add RegisterViewModel to Shared project

---
+++ Asp.Net.Identity.shared/RegisterViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Asp.Net.Identity.shared;
+
+public class RegisterViewModel
+{
+    [Required]
+    [StringLength(50)]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    [StringLength(50, MinimumLength = 8)]
+    public string Password { get; set; }
+
+    [Required]
+    [StringLength(50, MinimumLength = 8)]
+    public string ConfirmPassword { get; set; }
+}
