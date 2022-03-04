c6ab7e1b89e5d3ca9e833dd9b2d5b6fa8aa1c47a
Mohammad Zarif <m.zarif1996@gmail.com>
2022-03-04 18:29:53 +0330

    AddIdentity to projects

---
+++ Asp.Net.Identity.api/Models/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Asp.Net.Identity.api.Models;
+
+public class AppDbContext : IdentityDbContext
+{
+
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+
+    }
+
+    public DbSet<Employee> Employees { get; set; }
+}
+
--- Asp.Net.Identity.api/Program.cs
+++ Asp.Net.Identity.api/Program.cs
@@ -1,5 +1,5 @@
 using Asp.Net.Identity.api.Models;
-using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +11,15 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 
 });
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    options.Password.RequiredLength = 8;
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+}).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
