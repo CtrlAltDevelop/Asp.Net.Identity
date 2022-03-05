9231ffaf9d58fdfe135ac7d6c3d59d845e076702
Mohammad Zarif <m.zarif1996@gmail.com>
2022-03-05 17:09:32 +0330

    add AuthController to project for register new user

---
+++ Asp.Net.Identity.api/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using Asp.Net.Identity.api.Services;
+using Asp.Net.Identity.shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.Net.Identity.api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private IUserService _userService;
+
+    public AuthController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpPost("Register")]
+    public async Task<IActionResult> RegisterAsync([FromBody]RegisterViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var result = await _userService.RegisterUserAsync(model);
+            if (result.IsSuccess)
+                return Ok(result);
+        }
+
+        return BadRequest("Some properties are not valid");
+    }
+}
