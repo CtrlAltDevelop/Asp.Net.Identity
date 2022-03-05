e624437470ac0727406fcf2cc13c3c326b58027e
Mohammad Zarif <m.zarif1996@gmail.com>
2022-03-05 16:22:17 +0330

    complete IUserService and UserService

--- Asp.Net.Identity.api/Services/IUserService.cs
+++ Asp.Net.Identity.api/Services/IUserService.cs
@@ -1,12 +1,56 @@
-﻿namespace Asp.Net.Identity.api.Services;
+﻿using Asp.Net.Identity.shared;
+using Microsoft.AspNetCore.Identity;
+
+namespace Asp.Net.Identity.api.Services;
 
 public interface IUserService
 {
-
+    Task<UserManagerResponse> RegisterUserAsync(RegisterViewModel model);
 }
 
 public class UserService : IUserService
 {
+    private UserManager<IdentityUser> _userManager;
 
+    public UserService(UserManager<IdentityUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    public async Task<UserManagerResponse> RegisterUserAsync(RegisterViewModel model)
+    {
+        if (model == null)
+            throw new NullReferenceException("Register Model is null");
+
+        if (model.Password != model.ConfirmPassword)
+            return new UserManagerResponse
+            {
+                Message = "Confirm Password doesn't match the Password",
+                IsSuccess = false
+            };
+
+        var identityUser = new IdentityUser
+        {
+            Email = model.Email,
+            UserName = model.Email,
+        };
+
+        var result = await _userManager.CreateAsync(identityUser, model.Password);
+
+        if (result.Succeeded)
+        {
+            return new UserManagerResponse
+            {
+                Message = "User Created successfully!",
+                IsSuccess = true
+            };
+        }
+
+        return new UserManagerResponse
+        {
+            Message = "User did not create",
+            IsSuccess = false,
+            Error = result.Errors.Select(e =>e.Description)
+        };
+    }
 }
-
