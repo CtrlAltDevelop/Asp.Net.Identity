8a2e548ae8775fc9d68c26091519dbd9d4de8c03
Mohammad Zarif <m.zarif1996@gmail.com>
2022-03-07 00:08:12 +0330

    create login view model

---
+++ Asp.Net.Identity.shared/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Asp.Net.Identity.shared;
+
+public class LoginViewModel
+{
+    [Required]
+    [StringLength(50)]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    [StringLength(50, MinimumLength = 8)]
+    public string Password { get; set; }
+}
