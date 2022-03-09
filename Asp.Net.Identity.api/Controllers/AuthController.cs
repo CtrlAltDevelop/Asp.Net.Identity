82e8c6adb5c931ee4946c1905e8a713517265ab3
Mohammad Zarif <m.zarif1996@gmail.com>
2022-03-09 11:17:18 +0330

    add logon controller to project

--- Asp.Net.Identity.api/Controllers/AuthController.cs
+++ Asp.Net.Identity.api/Controllers/AuthController.cs
@@ -23,6 +23,23 @@ public class AuthController : ControllerBase
             var result = await _userService.RegisterUserAsync(model);
             if (result.IsSuccess)
                 return Ok(result);
+
+            return BadRequest(result);
+        }
+
+        return BadRequest("Some properties are not valid");
+    }
+
+    [HttpPost("Login")]
+    public async Task<IActionResult> LoginAsync([FromBody]LoginViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var result = await _userService.LoginUserAsync(model);
+            if (result.IsSuccess)
+                return Ok(result);
+
+            return BadRequest(result);
         }
 
         return BadRequest("Some properties are not valid");
